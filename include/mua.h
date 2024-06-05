862106ea22c01df38af5b2bccb55036192bad91f
Nicolas San Martin <nsanmartin@dc.uba.ar>
2024-06-04 22:05:08 -0300

    Add mua_ptr

--- include/mua.h
+++ include/mua.h
@@ -3,11 +3,16 @@
 #include <stdio.h>
 
 #define typedef_mua(Type) typedef struct { Type* items; size_t len; size_t capacity; } mua_name(Type)
-#define mua_at(Mua, Ix, Lval) ((Ix >= (Mua)->len) ? (value=-1) : ((Lval)=_mua_at(Mua, Ix), 0))
+#define typedef_mua_ptr(Type) \
+	typedef struct { Type** items; size_t len; size_t capacity; } mua_name_ptr(Type)
+
+#define mua_at(Mua, Ix, Lval) ((Ix >= (Mua)->len) ? (-1) : ((*Lval)=_mua_at(Mua, Ix), 0))
 #define mua_create(Type) (mua_name(Type)){0}
+#define mua_ptr_create(Type) (mua_name_ptr(Type)){0}
 #define mua_err(Mua) ((Mua)->capacity == 0 && (Mua)->len == 1 )
 #define mua_len(Mua) ((Mua)->len)
 #define mua_name(Type) mua_##Type
+#define mua_name_ptr(Type) mua_##Type##_ptr
 #define mua_append(Mua, Elem) do { \
                 if ((Mua)->len >= (Mua)->capacity) { _mua_realloc(Mua); } \
                 if (!mua_err(Mua)) _mua_at(Mua, (Mua)->len++) = Elem; \
--- test_mua.c
+++ test_mua.c
@@ -1,4 +1,5 @@
 #include <mua.h>
+#include <string.h>
 
 #define RESET   "\033[0m"
 #define RED     "\033[31m"
@@ -13,19 +14,23 @@ enum { TestOk = 0, TestFail = 1 };
     utest_assert(strcmp(Expected, String) == 0)
 
 typedef_mua(int);
+typedef struct { int x, y; float f; } Foo;
+typedef_mua(Foo);
+typedef_mua_ptr(char);
+typedef_mua_ptr(Foo);
 
 int test_mua_0(void) {
 	mua_int* x = &mua_create(int);
 	mua_append(x, 1);
 	if (mua_err(x)) { return 1; }
 	int value;
-	int e = mua_at(x, 0, value);
+	int e = mua_at(x, 0, &value);
 	if (e) { return 1; }
 	if (value != 1) {
 		return 1;
 	}
 
-	e = mua_at(x, 9, value);
+	e = mua_at(x, 9, &value);
 	if (!e) { return 1; }
 	return 0;
 }
@@ -40,11 +45,37 @@ int test_mua_1(void) {
 	utest_assert(mua_len(x) == test_len);
 	for (size_t i = 0; !mua_err(x) && i < test_len; ++i) {
 		int value;
-		int e = mua_at(x, i, value);
+		int e = mua_at(x, i, &value);
 		utest_assert(!e);
 		utest_assert(value == 3*(int)i);
 	}
 	
+	return 0;
+}
+
+
+int test_mua_2(void) {
+	mua_Foo* x = &mua_create(Foo);
+	utest_assert(mua_len(x) == 0);
+	mua_append(x, ((Foo){.x=4,.y=7,.f=0}));
+	utest_assert(!mua_err(x));
+	Foo f;
+	utest_assert(!mua_at(x, 0, &f));
+	utest_assert(f.x == 4 && f.y == 7 && f.f == 0);
+	return 0;
+}
+
+int test_mua_string(void) {
+	mua_char_ptr* x = &mua_ptr_create(char);
+	mua_append(x, "foo");
+	utest_assert(!mua_err(x));
+	mua_append(x, "bar");
+	utest_assert(!mua_err(x));
+	char* v;
+	utest_assert(!mua_at(x, 0, &v));
+	utest_assert_str_eq("foo", v);
+	utest_assert(!mua_at(x, 1, &v));
+	utest_assert_str_eq("bar", v);
 	return 0;
 }
 
